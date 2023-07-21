8b132ffc50451ab21909e7bc9770404aa8aed7f4
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-07-21 20:15:55 +0200

    Create NetworkClientsModule.kt

---
+++ app/src/main/java/com/reem/currencyconverter/app/di/NetworkClientsModule.kt
@@ -0,0 +1,21 @@
+package com.reem.currencyconverter.app.di
+
+import com.reem.currencyconverter.data.remote.networkLayer.client.FixerClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkClientsModule {
+
+    @Provides
+    @Singleton
+    fun provideFixerNetworkClient(retrofitBuilder: Retrofit.Builder): FixerClient {
+        return FixerClient(retrofitBuilder)
+    }
+
+}
