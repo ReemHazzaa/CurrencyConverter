7e8e2bcfb17f9ebc3cc904f98c12787aa4a4d016
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-07-22 00:46:50 +0200

    Update GetSymbolsUseCase.kt

--- app/src/main/java/com/reem/currencyconverter/domain/useCase/symbolsUseCase/GetSymbolsUseCase.kt
+++ app/src/main/java/com/reem/currencyconverter/domain/useCase/symbolsUseCase/GetSymbolsUseCase.kt
@@ -1,4 +1,14 @@
 package com.reem.currencyconverter.domain.useCase.symbolsUseCase
 
-class GetSymbolsUseCase {
+import com.reem.currencyconverter.domain.entity.symbols.SymbolsResponse
+import com.reem.currencyconverter.domain.repo.AppRepo
+import com.reem.currencyconverter.domain.useCase.baseUseCase.BaseUseCase
+import retrofit2.Response
+import javax.inject.Inject
+
+class GetSymbolsUseCase @Inject constructor(private val appRepo: AppRepo) :
+    BaseUseCase<Any, Response<SymbolsResponse>>() {
+    override suspend fun execute(params: Any?): Response<SymbolsResponse> {
+       return appRepo.getSymbols()
+    }
 }
