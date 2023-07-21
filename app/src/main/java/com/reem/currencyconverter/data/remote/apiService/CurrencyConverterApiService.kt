35d30636099d7635adb6ce1151dfda327ed80c00
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-07-21 19:19:27 +0200

    Added getSymbols function to API service interface

--- app/src/main/java/com/reem/currencyconverter/data/remote/apiService/CurrencyConverterApiService.kt
+++ app/src/main/java/com/reem/currencyconverter/data/remote/apiService/CurrencyConverterApiService.kt
@@ -1,4 +1,9 @@
 package com.reem.currencyconverter.data.remote.apiService
 
+import com.reem.currencyconverter.domain.entity.symbols.SymbolsResponse
+import retrofit2.http.GET
+
 interface CurrencyConverterApiService {
+    @GET("symbols")
+    suspend fun getSymbols(): SymbolsResponse
 }
