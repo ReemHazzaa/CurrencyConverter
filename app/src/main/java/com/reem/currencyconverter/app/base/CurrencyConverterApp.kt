a40ef9742c73cf1f4fdf853ac18270694b0d141a
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-07-21 23:26:53 +0200

    Added CurrencyConverterApp class

---
+++ app/src/main/java/com/reem/currencyconverter/app/base/CurrencyConverterApp.kt
@@ -0,0 +1,8 @@
+package com.reem.currencyconverter.app.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyConverterApp : Application()
+
