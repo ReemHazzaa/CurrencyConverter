e58e1bca4108ac9af92caff9cadbc7f6fbeb250b
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-07-22 22:56:01 +0200

    Added getRates to repo impl

--- app/src/main/java/com/reem/currencyconverter/data/remote/apiService/FixerApiService.kt
+++ app/src/main/java/com/reem/currencyconverter/data/remote/apiService/FixerApiService.kt
@@ -4,11 +4,16 @@ import com.reem.currencyconverter.domain.entity.rates.RatesResponse
 import com.reem.currencyconverter.domain.entity.symbols.SymbolsResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface FixerApiService {
 
     @GET("symbols")
     suspend fun getSymbols(): Response<SymbolsResponse>
+
     @GET("latest")
-    suspend fun getRates(): Response<RatesResponse>
+    suspend fun getRates(
+        @Query("base") base: String,
+        @Query("symbols") commaSeparatedSymbols: String
+    ): Response<RatesResponse>
 }
--- app/src/main/java/com/reem/currencyconverter/data/repo/AppRepoImpl.kt
+++ app/src/main/java/com/reem/currencyconverter/data/repo/AppRepoImpl.kt
@@ -1,6 +1,7 @@
 package com.reem.currencyconverter.data.repo
 
 import com.reem.currencyconverter.data.remote.apiService.FixerApiService
+import com.reem.currencyconverter.domain.entity.rates.RatesResponse
 import com.reem.currencyconverter.domain.entity.symbols.SymbolsResponse
 import com.reem.currencyconverter.domain.repo.AppRepo
 import retrofit2.Response
@@ -12,5 +13,12 @@ class AppRepoImpl @Inject constructor(
 
     override suspend fun getSymbols(): Response<SymbolsResponse> {
         return fixerApiService.getSymbols()
+    }
+
+    override suspend fun getRates(
+        base: String,
+        commaSeparatedSymbols: String
+    ): Response<RatesResponse> {
+        return fixerApiService.getRates(base, commaSeparatedSymbols)
     }
 }
