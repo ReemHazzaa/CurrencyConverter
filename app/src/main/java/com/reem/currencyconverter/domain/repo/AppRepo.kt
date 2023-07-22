534cf7e2684679690a03af606955465988e596af
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-07-22 22:46:23 +0200

    Added getRates function to the app repo

--- app/src/main/java/com/reem/currencyconverter/domain/repo/AppRepo.kt
+++ app/src/main/java/com/reem/currencyconverter/domain/repo/AppRepo.kt
@@ -1,8 +1,14 @@
 package com.reem.currencyconverter.domain.repo
 
+import com.reem.currencyconverter.domain.entity.rates.RatesResponse
 import com.reem.currencyconverter.domain.entity.symbols.SymbolsResponse
 import retrofit2.Response
 
 interface AppRepo {
     suspend fun getSymbols(): Response<SymbolsResponse>
+
+    suspend fun getRates(
+        base: String,
+        commaSeparatedSymbols: String
+    ): Response<RatesResponse>
 }
