9a0db5988307806e483efaf2fe3cf85e7cfdcebc
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-07-22 22:46:31 +0200

    Create GetRatesUseCase.kt

---
+++ app/src/main/java/com/reem/currencyconverter/domain/useCase/ratesUseCase/GetRatesUseCase.kt
@@ -0,0 +1,24 @@
+package com.reem.currencyconverter.domain.useCase.ratesUseCase
+
+import com.reem.currencyconverter.domain.entity.rates.RatesResponse
+import com.reem.currencyconverter.domain.repo.AppRepo
+import com.reem.currencyconverter.domain.useCase.baseUseCase.BaseUseCase
+import retrofit2.Response
+import javax.inject.Inject
+
+class GetRatesUseCase @Inject constructor(private val appRepo: AppRepo) :
+    BaseUseCase<GetRatesUseCase.Params, Response<RatesResponse>>() {
+
+    data class Params(
+        val base: String,
+        val commaSeparatedSymbols: String
+    )
+
+    override suspend fun execute(params: Params?): Response<RatesResponse> {
+        return appRepo.getRates(
+            base = params?.base ?: "EUR",
+            commaSeparatedSymbols = params?.commaSeparatedSymbols ?: "EGP"
+        )
+    }
+
+}
